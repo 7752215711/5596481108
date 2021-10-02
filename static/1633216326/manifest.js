f543c57b5736b766b112423140f249cf1cdb7a62
7752215711 <nghenhin622@gmail.com>
2021-10-03 06:54:54 +0700

    Update

--- static/1633216326/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fp\u002Fpost-first-post.html","\u002Fp\u002Fprivacy-policy.html","\u002Fp\u002Findex.html","\u002Fp\u002Fposts.html","\u002Fp\u002Fabout.html","\u002Fp\u002Fpost-my-day.html","\u002Fp\u002Fcontact-us.html"]})
---
+++ static/1633218813/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fp\u002Fpost-first-post.html","\u002Fp\u002Fposts.html","\u002Fp\u002Fpost-my-day.html","\u002Fp\u002Findex.html","\u002Fp\u002Fabout.html","\u002Fp\u002Fprivacy-policy.html","\u002Fp\u002Fcontact-us.html"]})
